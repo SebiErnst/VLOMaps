774581f0fadf407c16139e358737bdf88959365a
Prowadzący <teacher@imac-teacher.local>
2016-12-14 19:47:30 +0100

    first working prototype of map app

--- VLOMaps/ViewController.swift
+++ VLOMaps/ViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myMapView.delegate = self
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+        print("Will start loading map")
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("Region did change")
+        print(mapView.region)
+        print(mapView.visibleMapRect)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        myMapView.setCenter(newLocation.coordinate, animated: true)
     }
 
 
